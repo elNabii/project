aba60e0cb369b1f2422a3495b788b6ef4be13f8e
Ivan <example@gmail.com>
2022-11-22 18:39:19 +0300

    first commint

---
+++ js/script.js
@@ -0,0 +1,38 @@
+
+//console.log(-4/0);
+//console.log('string'+ 'fff');
+
+
+//const obj = {
+   // name: 'John',
+   // age: 25,
+    //isMarried: false
+//};
+//console.log(obj.name);
+
+//let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+//console.log(arr[1]);
+
+//const far = [1, 2, 3];
+
+//alert('hello')
+// const result = confirm('are youhear?');
+ //console.log(result);
+// const answer = +prompt('vam est 18 let?', '18');
+ //console.log(answer + 5);
+
+ //const answers = [];
+
+ //answers[0] = prompt('Ваше Имя', '');
+// answers[1] = prompt('Ваша Фамилия', '');
+ //answers[2] = prompt('Сколько вам лет?', '');
+
+ //console.log(typeof(answers));
+
+ //ИНТРОПОЛЯЦИЯ
+
+ const category = 'toys';
+
+ console.log('https//someurl.com/' + category)
+
+ console.log(`https//someurl.com/${category}/5`);
